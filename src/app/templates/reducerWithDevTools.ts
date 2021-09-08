227eab23ccaf7e9947b464fae7e1c79e392c2311
adam clark <aclark64@gmail.com>
2021-09-08 12:01:52 -0600

    Update reducerWithDevTools.ts

--- src/app/templates/reducerWithDevTools.ts
+++ src/app/templates/reducerWithDevTools.ts
@@ -1,8 +1,10 @@
 import {
     SET_COUNT
 } from './actions';
+import { withDevTools, devTools } from './devtools';
 
 export const reducer = (state: Record<string, any>, action: any) => {
+    if (withDevTools) devTools.send(action.type, state);
     switch (action.type) {
         case SET_COUNT:
             return {
